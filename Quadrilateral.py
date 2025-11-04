0e45d24ed6e50f7cf011acaf50366b5d407f58b7
Prithi22 <153989344+Prithi22@users.noreply.github.com>
2025-11-05 00:36:35 +0530

    Add files via upload

---
+++ Quadrilateral.py
@@ -0,0 +1,32 @@
+import csv
+import random
+
+def Quadgen(P1,P2,P3,P4):
+    tria_a = Triagen(P1,P2,P3)
+    tria_b = Triagen(P3,P4,P1)
+    with open("./Text.csv",mode = 'w',newline='') as file:
+        WriterObj = csv.writer(file)
+        for i in range(1000):
+            WriterObj.writerow(next(tria_a))
+
+def Triagen(P1,P2,P3):
+
+    while(True):
+        U = random.random()
+        V = random.random()
+
+        alpha = 1 - U - V
+        beta = U
+        gamma = V
+
+        X = alpha*P1[0] + beta*P2[0] + gamma*P3[0]
+        Y = alpha*P1[1] + beta*P2[1] + gamma*P3[1]
+        Z = alpha*P1[2] + beta*P2[2] + gamma*P3[2]
+
+        yield X,Y,Z
+
+Quadgen([0,0,0], [1,0,0], [1,1,0], [0,1,0])
+
+
+
+
